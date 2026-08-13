032854c97f1532fcb7abcd7b35bbcd04b7725a8d
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-08-12 20:46:58 -0700

    Add Linux endian compatibility header

---
+++ native/glibc_endian_compat.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <endian.h>
+#ifndef __BYTE_ORDER
+#define __BYTE_ORDER __BYTE_ORDER__
+#endif
+#ifndef __LITTLE_ENDIAN
+#define __LITTLE_ENDIAN __ORDER_LITTLE_ENDIAN__
+#endif
+#ifndef __BIG_ENDIAN
+#define __BIG_ENDIAN __ORDER_BIG_ENDIAN__
+#endif
