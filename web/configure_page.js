7e4866ea385c40dfeb9ec22fbd12a1a328db0a59
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-08-14 20:19:13 -0700

    Restore Blocks and Payouts nav links

--- web/configure_page.js
+++ web/configure_page.js
@@ -50,7 +50,7 @@
   function installNav(){
     const nav=document.querySelector('header nav');
     if(!nav) return;
-    nav.innerHTML='<a href="/">Dashboard</a><a href="/miners">Miners</a><a href="/configure">Configure</a>';
+    nav.innerHTML='<a href="/">Dashboard</a><a href="/miners">Miners</a><a href="/blocks">Blocks</a><a href="/payouts">Payouts</a><a href="/configure">Configure</a>';
   }
 
   function blockLegacyHomeCommands(){
