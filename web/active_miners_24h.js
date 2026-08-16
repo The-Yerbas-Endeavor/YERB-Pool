d99d8b9fd5bc11841f75863e22d9e9762cf98086
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-08-16 12:00:11 -0700

    Hide miners inactive for 24 hours from public lists

---
+++ web/active_miners_24h.js
@@ -0,0 +1,21 @@
+(function(){
+  const ACTIVE_WINDOW_SECONDS=24*60*60;
+
+  function recentlyActive(item){
+    return Number(item?.last_seen_at||0) >= Math.floor(Date.now()/1000)-ACTIVE_WINDOW_SECONDS;
+  }
+
+  const originalRenderMiners=window.renderMiners;
+  if(typeof originalRenderMiners==='function'){
+    window.renderMiners=function(miners){
+      return originalRenderMiners((miners||[]).filter(recentlyActive));
+    };
+  }
+
+  const originalWorkerBars=window.workerBars;
+  if(typeof originalWorkerBars==='function'){
+    window.workerBars=function(workers){
+      return originalWorkerBars((workers||[]).filter(recentlyActive));
+    };
+  }
+})();
